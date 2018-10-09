8e3a84b74c999f9b4e8d0c7a50b860b8298f4eb5
felipe <marcelus20felipe@gmail.com>
2018-10-09 09:47:41 +0100

    linked serializers with viewsets

--- income_management/views.py
+++ income_management/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Users, Transactions, Description
+from .serializers import UsersSerializers as u, TransactionsSerializer as t, DescriptionSerializer as d
 
 # Create your views here.
+class DescriptionView (viewsets.ModelViewSet):
+    queryset = Description.objects.all()
+    serializer_class = d
+
+class TransactionView (viewsets.ModelViewSet):
+    queryset = Transactions.objects.all()
+    serializer_class = t
+
+class UsersView (viewsets.ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = u
