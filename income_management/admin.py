2e4f1ed730728f87342671ba0d6a55e41f6fef8d
felipe <marcelus20felipe@gmail.com>
2018-10-09 09:45:05 +0100

    included the Models classes to appear in /admin routing

--- income_management/admin.py
+++ income_management/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Transactions, Users, Description
 
 # Register your models here.
+
+admin.site.register(Transactions)
+admin.site.register(Users)
+admin.site.register(Description)
